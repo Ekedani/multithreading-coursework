a965108335488bfb762231337b9652f20c746f6a
Vladlen <ilusenkovladlen@gmail.com>
2023-05-09 18:33:49 +0300

    Added comments for DisjointSetUnion

--- src/DisjointSetUnion.py
+++ src/DisjointSetUnion.py
@@ -4,11 +4,20 @@ class DisjointSetUnion:
         self.rank = {}
 
     def makeSet(self, element):
+        """
+        Creates a new subset
+        :param element: parent of the new subset
+        """
         if element not in self.parent:
             self.parent[element] = element
             self.rank[element] = 0
 
     def find(self, element):
+        """
+        Searches for the subset, which contains an element
+        :param element: the element to be searched
+        :return: parent of the subset
+        """
         if element not in self.parent:
             return None
         if self.parent[element] != element:
@@ -16,6 +25,11 @@ class DisjointSetUnion:
         return self.parent[element]
 
     def union(self, subsetA, subsetB):
+        """
+        Joins two subsets into one with the highest rank
+        :param subsetA: first subset to be joined
+        :param subsetB: second subset to be joined
+        """
         rootA = self.find(subsetA)
         rootB = self.find(subsetB)
         if rootA is None or rootB is None or rootA == rootB:
