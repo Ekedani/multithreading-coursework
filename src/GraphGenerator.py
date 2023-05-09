790dc88cefdad3feba326de2665e0f28e3603f0d
Vladlen <ilusenkovladlen@gmail.com>
2023-05-09 19:03:16 +0300

    Revert "Added 9 complete graphs for testing"
    This reverts commit 7f4849108d2974b55eececdb7d81e030251233a6.

--- src/GraphGenerator.py
+++ src/GraphGenerator.py
@@ -5,11 +5,11 @@ from src.Graph import Graph
 
 class GraphGenerator:
     @staticmethod
-    def generateComplete(vertices_num, high) -> Graph:
+    def generateComplete(vertices_num) -> Graph:
         graph_matrix = [[0 for _ in range(vertices_num)] for _ in range(vertices_num)]
         for i in range(vertices_num):
             for j in range(i + 1, vertices_num):
-                weight = random.randint(1, high)
+                weight = random.randint(1, 10000)
                 graph_matrix[i][j] = weight
                 graph_matrix[j][i] = weight
         return Graph.fromMatrix(graph_matrix)
