a6686232204f96e0ede323bd0f9cb9b6999c26aa
Vladlen <ilusenkovladlen@gmail.com>
2023-05-15 12:29:41 +0300

    Expanded testing module

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import time
-
+from src import TestingHelper
 from src.Graph import Graph
 from src.GraphGenerator import GraphGenerator
 from src.SerialKruskalAlgorithm import SerialKruskalAlgorithm
 from src.ParallelKruskalAlgorithm import ParallelKruskalAlgorithm
-from src.TestingHelper import TestingHelper
+
 
 VISUALIZE_RESULTS = False
 
 if __name__ == '__main__':
+    TestingHelper.runPredefinedTests([250, 500, 750, 1000, 1250, 1500, 1750, 2000])
     # graph = Graph.readFromFile('2000')
-    graph = GraphGenerator.generateComplete(2000, 100000)
-    serialKruskalSolver = SerialKruskalAlgorithm()
-    parallelKruskalSolver = ParallelKruskalAlgorithm(12)
+    # graph = GraphGenerator.generateComplete(2000, 100000)
+    # serialKruskalSolver = SerialKruskalAlgorithm()
+    # parallelKruskalSolver = ParallelKruskalAlgorithm(8)
 
-    serial_start = time.time()
-    mst_ser = serialKruskalSolver.findMinimumSpanningTree(graph)
-    serial_end = time.time()
-    print(f'Total serial time: {serial_end - serial_start}')
-    print(mst_ser.getWeight())
+    # serial_start = time.time()
+    # mst_ser = serialKruskalSolver.findMinimumSpanningTree(graph)
+    # serial_end = time.time()
+    # print(f'Total serial time: {serial_end - serial_start}')
+    # print(mst_ser.getWeight())
 
     # parallel_start = time.time()
     # mst_par = parallelKruskalSolver.findMinimumSpanningTree(graph)
@@ -26,7 +27,7 @@ if __name__ == '__main__':
     # print(f'Total parallel time: {parallel_end - parallel_start}')
     # print(mst_par.getWeight())
 
-    if VISUALIZE_RESULTS:
-        TestingHelper.renderGraph(graph, 'original', node_color='green')
-        TestingHelper.renderGraph(mst_ser, 'mst_ser', node_color='red')
-        # TestingHelper.renderGraph(mst_par, 'mst_par', node_color='blue')
+    # if VISUALIZE_RESULTS:
+    #     TestingHelper.renderGraph(graph, 'original', node_color='green')
+    #     TestingHelper.renderGraph(mst_ser, 'mst_ser', node_color='red')
+    #     # TestingHelper.renderGraph(mst_par, 'mst_par', node_color='blue')
--- src/TestingHelper.py
+++ src/TestingHelper.py
@@ -1,54 +1,117 @@
+import os
+import time
+
 import networkx as nx
 import matplotlib.pyplot as plt
+import csv
 
 from src.Graph import Graph
+from src.GraphGenerator import GraphGenerator
+from src.ParallelKruskalAlgorithm import ParallelKruskalAlgorithm
+from src.SerialKruskalAlgorithm import SerialKruskalAlgorithm
 
 
-class TestingHelper:
-    @staticmethod
-    def hasMinimumSpanningTreeWeight(graph: Graph, mst: Graph) -> bool:
-        """
-        Checks if given MST of the graph is correct using networkx library
-        :param graph: graph to be checked
-        :param mst: MST to be checked
-        :return: True if MST is correct, False if MST is incorrect
-        """
-        nx_graph = nx.Graph()
-        weight_matrix = graph.toMatrix()
-        vertices = len(weight_matrix)
-        nx_graph.add_nodes_from(range(vertices))
-        for i in range(vertices):
-            for j in range(i + 1, vertices):
-                if weight_matrix[i][j] != 0:
-                    nx_graph.add_edge(i, j, weight=weight_matrix[i][j])
-        nx_mst = nx.minimum_spanning_tree(nx_graph, weight='weight', ignore_nan=False)
-        nx_mst_weight = sum(nx.get_edge_attributes(nx_mst, 'weight').values())
-        # Graph does not necessarily have one MST, but their weight is always the same
-        return nx_mst_weight == mst.getWeight()
+def renderGraph(graph: Graph, filename: str, node_color: str = 'blue') -> None:
+    """
+    Renders graph to file with given filename
+    :param graph: graph to be rendered
+    :param filename: destination file name
+    :param node_color:
+    """
+    nx_graph = nx.Graph()
+    weight_matrix = graph.toMatrix()
+    vertices = len(weight_matrix)
+    nx_graph.add_nodes_from(range(vertices))
+    for i in range(vertices):
+        for j in range(i + 1, vertices):
+            if weight_matrix[i][j] != 0:
+                nx_graph.add_edge(i, j, weight=weight_matrix[i][j])
 
-    @staticmethod
-    def renderGraph(graph: Graph, filename: str, node_color: str = 'blue') -> None:
-        """
-        Renders graph to file with given filename
-        :param graph: graph to be rendered
-        :param filename: destination file name
-        :param node_color:
-        """
-        nx_graph = nx.Graph()
-        weight_matrix = graph.toMatrix()
-        vertices = len(weight_matrix)
-        nx_graph.add_nodes_from(range(vertices))
-        for i in range(vertices):
-            for j in range(i + 1, vertices):
-                if weight_matrix[i][j] != 0:
-                    nx_graph.add_edge(i, j, weight=weight_matrix[i][j])
+    pos = nx.circular_layout(nx_graph)
+    nx.draw(nx_graph, pos=pos, edgecolors='k', with_labels=False, node_color=f'tab:{node_color}')
+    edges = len(nx_graph.edges())
+    vertices = len(nx_graph.nodes())
+    graph_weight = sum(nx.get_edge_attributes(nx_graph, 'weight').values())
+    plt.text(0.95, 0.05, f"Edges: {edges}\nVertices: {vertices}\nGraph weight: {graph_weight}",
+             transform=plt.gcf().transFigure, fontsize=10, ha='right', va='bottom')
+    plt.savefig(f'{filename}.png')
+    plt.close()
 
-        pos = nx.circular_layout(nx_graph)
-        nx.draw(nx_graph, pos=pos, edgecolors='k', with_labels=False, node_color=f'tab:{node_color}')
-        edges = len(nx_graph.edges())
-        vertices = len(nx_graph.nodes())
-        graph_weight = sum(nx.get_edge_attributes(nx_graph, 'weight').values())
-        plt.text(0.95, 0.05, f"Edges: {edges}\nVertices: {vertices}\nGraph weight: {graph_weight}",
-                 transform=plt.gcf().transFigure, fontsize=10, ha='right', va='bottom')
-        plt.savefig(f'{filename}.png')
-        plt.close()
+
+def generatePredefinedTests(test_sizes: list, high: int):
+    data_directory = 'data'
+    for size in test_sizes:
+        test_graph = GraphGenerator.generateComplete(size, high)
+        test_graph.saveToFile(str(size))
+        nx_mst = findMinimumSpanningTreeWithNX(test_graph)
+        mst_weight = sum(nx.get_edge_attributes(nx_mst, 'weight').values())
+        filepath = os.path.join(data_directory, str(size) + "_solution.csv")
+        with open(filepath, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([mst_weight])
+
+
+def runPredefinedTests(test_sizes: list, parallelism: int = 8, show_time: bool = True):
+    data_directory = 'data'
+    failed_tests = []
+    serialKruskalSolver = SerialKruskalAlgorithm()
+    parallelKruskalSolver = ParallelKruskalAlgorithm(parallelism)
+    for size in test_sizes:
+        print(f'===== TEST FOR SIZE {size} =====')
+        test_graph = Graph.readFromFile(str(size))
+        serial_start = time.time()
+        serial_mst = serialKruskalSolver.findMinimumSpanningTree(graph=test_graph)
+        serial_end = time.time()
+        parallel_start = time.time()
+        parallel_mst = parallelKruskalSolver.findMinimumSpanningTree(graph=test_graph)
+        parallel_end = time.time()
+        if show_time:
+            print(f'Total serial time: {serial_end - serial_start}')
+            print(f'Total parallel time: {parallel_end - parallel_start}')
+        filepath = os.path.join(data_directory, str(size) + "_solution.csv")
+        with open(filepath, 'r') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            nx_weight = float(rows[0][0])
+        serial_is_correct = nx_weight == serial_mst.getWeight()
+        parallel_is_correct = nx_weight == parallel_mst.getWeight()
+        print(f'Serial is correct: {serial_is_correct}')
+        print(f'Parallel is correct: {parallel_is_correct}')
+        if not (serial_is_correct and parallel_is_correct):
+            failed_tests.append(size)
+    if len(failed_tests) == 0:
+        print('None of tests failed.')
+
+
+def hasMinimumSpanningTreeWeight(graph: Graph, mst: Graph) -> bool:
+    """
+    Checks if given MST of the graph is correct using networkx library
+    :param graph: graph to be checked
+    :param mst: MST to be checked
+    :return: True if MST is correct, False if MST is incorrect
+    """
+    nx_graph = nx.Graph()
+    weight_matrix = graph.toMatrix()
+    vertices = len(weight_matrix)
+    nx_graph.add_nodes_from(range(vertices))
+    for i in range(vertices):
+        for j in range(i + 1, vertices):
+            if weight_matrix[i][j] != 0:
+                nx_graph.add_edge(i, j, weight=weight_matrix[i][j])
+    nx_mst = nx.minimum_spanning_tree(nx_graph, weight='weight', ignore_nan=False)
+    nx_mst_weight = sum(nx.get_edge_attributes(nx_mst, 'weight').values())
+    # Graph does not necessarily have one MST, but their weight is always the same
+    return nx_mst_weight == mst.getWeight()
+
+
+def findMinimumSpanningTreeWithNX(graph: Graph):
+    nx_graph = nx.Graph()
+    weight_matrix = graph.toMatrix()
+    vertices = len(weight_matrix)
+    nx_graph.add_nodes_from(range(vertices))
+    for i in range(vertices):
+        for j in range(i + 1, vertices):
+            if weight_matrix[i][j] != 0:
+                nx_graph.add_edge(i, j, weight=weight_matrix[i][j])
+    nx_mst = nx.minimum_spanning_tree(nx_graph, weight='weight', ignore_nan=False)
+    return nx_mst
